85c2920aae8ad5efd509a54317fd2fa9cef575ab
jtr1k <vip.banan.00@mail.ru>
2024-08-22 23:14:34 +0300

    Second commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from fastapi import Header
 from fastapi import UploadFile
 from fastapi import File
 from fastapi.templating import Jinja2Templates
+import random
 
 from modules.GetFrameFromVideo import *
 from modules.GetVideoFrame import *
@@ -31,53 +32,67 @@ templates = Jinja2Templates(directory="templates")
 DEFAULTPATH = "/home/jtr1k/drivehack/drivehack-backend"
 PathToFrame = DEFAULTPATH + "/preview/"
 VideoPath = DEFAULTPATH + "/video/"
-VideoCapture = cv2.VideoCapture(VideoPath + "SampleVideo_1280x720_20mb.mp4")
-CHUNK_SIZE = VideoCapture.get(cv2.CAP_PROP_FRAME_WIDTH) * VideoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 sessions = {} 
 
 @app.get("/api/video/watch")
-async def video_endpoint(request: Request, range: str = Header(None)):
+async def video_endpoint(response: Response, request: Request, range: str = Header(None)):
     cookie = request.cookies.get("cookie")
+    
     if cookie == None:
         return Response("No files uploaded", status_code=500)
+    
+
+    filename = sessions[cookie]
+    filesize = str(filename.stat().st_size)
+
+    WIDTH, HEIGHT = GetChunkSize(filename)
+    CHUNK_SIZE = WIDTH * HEIGHT
+
     start, end = range.replace("bytes=", "").split("-")
     start = int(start)
     end = int(end) if end else start + CHUNK_SIZE
-    filename = sessions[cookie]
-    filesize = str(filename.stat().st_size)
     data = GetVideoFrame(filename, start, end) 
+    
     headers = {
         'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
         'Accept-Ranges': 'bytes'
     }
+    
     return Response(data, status_code=206, headers=headers,media_type="video/mp4")
 
 @app.post("/api/video/upload")
-async def Upload(request: Request, file: Annotated[bytes, File()]):
+async def Upload(response: Response, request: Request, file: Annotated[bytes, File()]):
     cookie = request.cookies.get("cookie")
     if (cookie == None):
-        user_uuid = uuid4()
+        user_uuid = str(uuid4())
         sessions[user_uuid] = ""
         response.set_cookie(key = "cookie", value = user_uuid)
-        return response
+        cookie = user_uuid
     filename = Path(VideoPath + RandomString())
-    print("SVOZZZ")
+    print(cookie)
     sessions[cookie] = filename
+    print(filename)
+    print(cookie)
     UploadedFile = open(filename, "wb")
     UploadedFile.write(file)
     UploadedFile.close()
+    return "OK"
 
 
 
 @app.get("/api/video/preview")
 async def Preview(request: Request):
     cookie = request.cookies.get("cookie")
-    FramePath = GetFrameFromVideo(VideoPath, PathToFrame)
+    if (cookie == None):
+        return "No file uploaded"
+    VideoFilePath = sessions[cookie]
+    FramePath = GetFrameFromVideo(VideoFilePath, PathToFrame)
     PreviewFile = open(FramePath, "rb").read()
     return Response(content=PreviewFile, media_type="image/jpg")
 
     
 @app.post("/api/render/lines")
-async def GetLines(lines: Lines):
+async def GetLines(request: Request, lines: Lines):
     answer = ConvertLinesToArray(lines.lines)
     # connect with ML model
--- modules/GetVideoFrame.py
+++ modules/GetVideoFrame.py
@@ -1,6 +1,14 @@
+import cv2
 
 def GetVideoFrame(VideoPath, start, end):
     with open(VideoPath, "rb") as video:
         video.seek(start)
         data = video.read(int(end) - int(start))
         return data
+
+
+def GetChunkSize(PathToFile):
+    VideoCapture = cv2.VideoCapture(PathToFile)
+    WIDTH = VideoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    HEIGHT = VideoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    return [WIDTH, HEIGHT]
