dfe627a6017df7b2804c595aee18e2621412089a
Anton Egorov <a12286@yandex.com>
2024-08-23 04:00:13 +0300

    WIP add model interaction

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from fastapi import FastAPI
-from fastapi import Request, Response
+from fastapi import Request, Response, HTTPException
 from fastapi import Header
 from fastapi import UploadFile
 from fastapi import File
 from fastapi.templating import Jinja2Templates
+
+from subprocess import Popen
 
 from modules.GetFrameFromVideo import *
 from modules.GetVideoFrame import *
@@ -17,22 +19,27 @@ from uuid import uuid4
 
 from typing import Annotated
 
+import os
+
+MODEL_RUN = ['/home/ninvoido/processor/venv/bin/python3', '/home/ninvoido/processor/lib.py', '--no-tqdm']
+
 app = FastAPI()
+
 templates = Jinja2Templates(directory="templates")
 
-DEFAULTPATH = "/home/jtr1k/drivehack/drivehack-backend"
+DEFAULTPATH = os.path.dirname(__file__)
 PathToFrame = DEFAULTPATH + "/preview/"
 VideoPath = DEFAULTPATH + "/video/"
 
-sessions = {} 
+sessions = {}
 
 @app.get("/api/video/watch")
 async def video_endpoint(response: Response, request: Request, range: str = Header(None)):
     cookie = request.cookies.get("cookie")
-    
-    if cookie == None:
+
+    if cookie is None:
         return Response("No files uploaded", status_code=500)
-    
+
 
     filename = sessions[cookie]
     filesize = str(filename.stat().st_size)
@@ -43,19 +50,19 @@ async def video_endpoint(response: Response, request: Request, range: str = Head
     start, end = range.replace("bytes=", "").split("-")
     start = int(start)
     end = int(end) if end else start + CHUNK_SIZE
-    data = GetVideoFrame(filename, start, end) 
-    
+    data = GetVideoFrame(filename, start, end)
+
     headers = {
         'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
         'Accept-Ranges': 'bytes'
     }
-    
+
     return Response(data, status_code=206, headers=headers,media_type="video/mp4")
 
 @app.post("/api/video/upload")
 async def Upload(response: Response, request: Request, file: Annotated[bytes, File()]):
     cookie = request.cookies.get("cookie")
-    if (cookie == None):
+    if (cookie is None):
         user_uuid = str(uuid4())
         sessions[user_uuid] = ""
         response.set_cookie(key = "cookie", value = user_uuid)
@@ -75,20 +82,48 @@ async def Upload(response: Response, request: Request, file: Annotated[bytes, Fi
 @app.get("/api/video/preview")
 async def Preview(request: Request):
     cookie = request.cookies.get("cookie")
-    if (cookie == None):
+    if (cookie is None):
         return "No file uploaded"
     VideoFilePath = sessions[cookie]
     FramePath = GetFrameFromVideo(VideoFilePath, PathToFrame)
     PreviewFile = open(FramePath, "rb").read()
     return Response(content=PreviewFile, media_type="image/jpg")
 
-    
+
+@app.get("/api/video/data")
+async def get_data(request: Request):
+    cookie = request.cookies.get("cookie")
+    if cookie is None:
+        return "You haven't sent any files"
+    # connect with ML model
+
+    filename_json = cookie + '.out.json'
+    if not os.path.exists(filename_json):
+        raise HTTPException(status_code=404)
+
+    with open(filename_json) as fp:
+        import json
+        return json.load(fp)
+
+
 @app.post("/api/render/lines")
 async def GetLines(request: Request, lines: Lines):
     cookie = request.cookies.get("cookie")
     print(lines.lines)
-    if cookie == None:
+    if cookie is None:
         return "You haven't sent any files"
     answer = ConvertLinesToArray(lines.lines)
     print(answer)
     # connect with ML model
+
+    with open(cookie + '.lines.json', 'w') as fp:
+        import json
+        json.dump(lines, fp)  # ENSURE FORMAT
+
+    filename_in = cookie
+    filename_out = cookie + '.out'
+    filename_json = cookie + '.out.json'
+
+    Popen(MODEL_RUN + ['-i', filename_in, '-o', filename_out, '-j', filename_json, '-L', cookie + '.lines.json'], shell=False)
+
+    return 'ok'
